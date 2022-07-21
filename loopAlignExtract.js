f6872fddda0d8a4b9704ad76fec5665e1988f2a9
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2022-07-21 16:23:35 -0700

    Update loopAlignExtract.js

--- loopAlignExtract.js
+++ loopAlignExtract.js
@@ -10,13 +10,15 @@ glob("FAVE-align/*.wav", function (er, files) {
     	console.log(sprintf("Currently aligning for %s...", fileName))
 
     	shelljs.cd("FAVE-align")
-    	shelljs.exec(sprintf("python FAAValign.py -v -i %s.wav %s.txt"), fileName, fileName)
+
+        let cmd = 
+    	shelljs.exec(sprintf("python FAAValign.py -v -i %s.wav %s.txt", fileName, fileName))
     	shelljs.cd("..")
 
     	console.log(sprintf("Currently extracting for %s...", fileName))
 
     	shelljs.cd("FAVE-extract")
-    	shelljs.exec(sprintf("python bin/FAAVextract.py -v -i ../FAVE-align/%s.wav ../FAVE-align/%s.TextGrid "), fileName, fileName)
+    	shelljs.exec(sprintf("python bin/FAAVextract.py -v -i ../FAVE-align/%s.wav ../FAVE-align/%s.TextGrid ", fileName, fileName))
     	shelljs.cd("..")
 
     }
