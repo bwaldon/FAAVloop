ace3107bc3fecf78f7d23e7979100003c291f732
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2022-07-21 16:40:23 -0700

    Update loopAlignExtract.js

--- loopAlignExtract.js
+++ loopAlignExtract.js
@@ -5,26 +5,25 @@ const sprintf = require('sprintf-js').sprintf
 glob("FAVE-align/*.wav", function (er, files) {
     for(var i = 0; i < files.length; i++) {
 
-    	let fileName = files[i].replace(/\.[^/.]+$/, "")
-    	
-    	console.log(sprintf("Currently aligning for %s...", fileName))
+        let fileName = files[i].replace(/\.[^/.]+$/, "")
+        fileName = fileName.replace("FAVE-align","")
 
-    	shelljs.cd("FAVE-align")
+        console.log(sprintf("Currently aligning for %s...", fileName))
+
+        shelljs.cd("FAVE-align")
         let cmd = sprintf("python FAAValign.py -v -i %s.wav %s.txt", fileName)
         console.log(sprintf("COMMAND: %s", cmd))
-    	shelljs.exec(cmd)
-    	shelljs.cd("..")
+        shelljs.exec(cmd)
+        shelljs.cd("..")
 
-    	console.log(sprintf("Currently extracting for %s...", fileName))
+        console.log(sprintf("Currently extracting for %s...", fileName))
 
-    	shelljs.cd("FAVE-extract")
+        shelljs.cd("FAVE-extract")
         cmd = sprintf("python bin/FAAVextract.py -v -i ../FAVE-align/%s.wav ../FAVE-align/%s.TextGrid ", fileName)
         console.log(sprintf("COMMAND: %s", cmd))
-    	shelljs.exec(cmd)
-    	shelljs.cd("..")
+        shelljs.exec(cmd)
+        shelljs.cd("..")
 
     }
 
 })
-
-
